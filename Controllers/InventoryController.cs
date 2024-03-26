366ef5ff6e45eb8fd2f209f445759f05df3f68b2
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-25 22:00:09 -0500

    Creating Controllers

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AdoWebApiStoreInventory.Repositories.Repositories;
+using AdoWebApiStoreInventory.Repositories.Models;
+
+namespace AdoWebAPIStoreInventory.Controllers
+{
+
+    [ApiController] // what does this do ?
+    [Route("[controller]")] // what does this do ?
+    public class InventoryController : ControllerBase
+    {
+            private readonly ILogger<InventoryController> _logger;
+            private readonly InventoryRepository _myRepository;
+
+            public InventoryController(ILogger<InventoryController> logger, InventoryRepository myRepository)
+            {
+                _logger = logger;
+                _myRepository = myRepository;
+            }
+
+            [HttpPost("Create")]
+            public async Task<IActionResult> Create()
+            {
+                return new OkObjectResult(await _myRepository.Create());
+            }
+    }
+}
